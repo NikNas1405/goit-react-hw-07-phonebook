9e28234fb7dccd7faa6e5fde2788f67107c33bba
Anastasiia Nikitina <103371039+NikNas1405@users.noreply.github.com>
2023-09-23 21:11:45 +0300

    Update contactsSlice.js

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { toast } from 'react-toastify';
 import { initialState } from '../initialState';
 import { fetchContacts, addContact, deleteContact } from 'utills/getAPI';
 
